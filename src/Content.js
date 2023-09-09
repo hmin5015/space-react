6991329f1fdf23b59bb8c63fb5946b9f694beb98
스터디하자 <hmin5015@gmail.com>
2023-09-08 21:00:50 -0700

    Add event handler example

--- src/Content.js
+++ src/Content.js
@@ -2,16 +2,31 @@ import React from 'react'
 
 const Content = () => {
   const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave'];
-    const index = Math.floor(Math.random() * 3);
+    const names = ['Joseph', 'Juna', 'Issac', 'Jacob', 'Raina'];
+    const index = Math.floor(Math.random() * 5);
     return names[index];
+  }
+
+  const handleClick = () => {
+    console.log('You clicked it')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
   }
 
   return (
     <main>
-      <p>
-        Hello {handleNameChange()}
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}!
       </p>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={() => handleClick2(handleNameChange())}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
