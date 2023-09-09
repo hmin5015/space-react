ebfb080de7c6f68c1c02b3226550ccafc2a9c0ed
스터디하자 <hmin5015@gmail.com>
2023-09-09 06:45:11 -0700

    Add function which is adding item

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
 import { useState } from 'react';
+import AddItem from './AddItem';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
 function App() {
-    const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+  const [newItem, setNewItem] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {
+      id, checked: false, item
+    };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
 
   const handleCheck = (id) => {
-    const listItems = items
-      .map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    
-    setItems(listItems);
-
-    // save list items in the local storage
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setAndSaveItems(listItems);
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
+    setAndSaveItems(listItems);
+  }
 
-    // save list items in the local storage
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  const handleSubmit = (e) => {
+    // prevent refresh page
+    e.preventDefault();
+
+    if (!newItem) return;
+    
+    addItem(newItem);
+    setNewItem('');
   }
 
   return (
     <div className="App">
       <Header title="Grocery List" />
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content 
         items={items}
         handleCheck={handleCheck}
